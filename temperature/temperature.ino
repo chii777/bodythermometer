0e97f78413f81248b0cf576fcede5fa579a775b0
chii777 <ma_ksks40x25@yahoo.co.jp>
2020-10-19 21:14:24 +0900

    complete

---
+++ temperature/temperature.ino
@@ -0,0 +1,69 @@
+#include <I2CLiquidCrystal.h>
+#include <mglcd.h>
+#include <Wire.h>
+#include<stdio.h>
+#include<math.h>
+#define num 100
+int i;
+float average;
+int vol;
+float real;
+float res;
+float temp;
+short tempr[num];
+float sum =0;
+
+I2CLiquidCrystal lcd(60, (bool)true);
+
+void setup() {
+Serial.begin(9600);
+digitalWrite(13,HIGH);
+
+lcd.begin(16, 1);
+lcd.kana("mami");
+delay(2000);
+lcd.clear();
+lcd.kana("body temperature");
+delay(2000);
+
+ vol = analogRead(A0);
+ real = vol*5.0/1023.0;  //voltage
+ res = 10000.0*real/(5.0-real); //resistor
+ temp = (1/(((1/3380.0)*log(res/10000))+(1/298.0)))-273.0;
+
+ 
+}
+
+
+void loop() {
+ sum = 0;
+ vol = analogRead(A0);
+ real = vol*5.0/1023.0;  //voltage
+ res = 10000.0*real/(5.0-real); //resistor
+ temp = (1/(((1/3380.0)*log(res/10000))+(1/298.0)))-273.0;
+
+lcd.clear();
+
+
+for (i = 9; i > 0; i--) {
+ if(temp > 35.0){
+   tempr[0] = temp;  // 温度を格納
+   tempr[i] = tempr[i-1];
+   Serial.println(tempr[i]);
+ }else{
+   tempr[i] = tempr[i];
+   Serial.println(tempr[i]);
+ } 
+}
+
+for (i = 0; i < 10;i++) {
+  sum += tempr[i];
+}
+
+average = sum / 10;
+
+lcd.print(average - 0.6);
+
+delay(1000);
+
+}
